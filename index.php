75de68d1efd9a0778989ad63acda4aab7362e44b
Afikfok <99673569+Afikfok@users.noreply.github.com>
2023-06-02 17:55:18 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+
+<title>Kissed by Mr.R07 Xploits</title>
+<center><body bgcolor="black"><font color="white">
+<pre>
+<br><br>
+<h1>Kissed By Mr.R07 Xploits</h1>
+<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/3J8EOeKLTLXORtWPpOU5bE?utm_source=generator&theme=0" width="30%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+
+<i><b></b></i>
+
+{Security Is Just An Illusion}
+
+-- Sh00tz ---
+<b>YOUR SISTEM XD</b> 
